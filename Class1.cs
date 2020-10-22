8e6e2563755ba1a44064794f4e06a5e5180b6f4c
Aleksandr Ponomarev <avp.ponomarev@gmail.com>
2020-10-22 21:29:12 +0300

    Добавьте файлы проекта.

---
+++ Class1.cs
@@ -0,0 +1,112 @@
+﻿using RobotOM;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spider_Free
+{
+   class Class1
+   {
+      public string[] NamesSectBar { get; private set; }
+      public string[] MaterialsRSA { get; private set; }
+
+      void getBarSectionNamesRSA(RobotApplication appRSA)
+      {
+         RobotLabelServer labelsRSA = appRSA.Project.Structure.Labels;
+         //RobotLabel labelRSA;
+         RobotNamesArray namArrRSA = labelsRSA.GetAvailableNames(IRobotLabelType.I_LT_BAR_SECTION);
+         string[] tmp = new string[namArrRSA.Count];
+         for (int i = 0; i < namArrRSA.Count; i++)
+         {
+            tmp[i] = namArrRSA.Get(i + 1);
+         }
+         NamesSectBar = tmp;
+      }
+
+      public void getBarSectionNamesRSA()
+      {
+         RobotApplication appRSA = new RobotApplication();
+         RobotLabelServer labelsRSA = appRSA.Project.Structure.Labels;
+         //RobotLabel labelRSA;
+         RobotNamesArray namArrRSA = labelsRSA.GetAvailableNames(IRobotLabelType.I_LT_BAR_SECTION);
+         string[] tmp = new string[namArrRSA.Count];
+         for (int i = 0; i < namArrRSA.Count; i++)
+         {
+            tmp[i] = namArrRSA.Get(i + 1);
+         }
+         NamesSectBar = tmp;
+      }
+
+      void getMaterialNameListRSA(RobotApplication appRSA)
+      {
+         RobotLabelServer labelsRSA = appRSA.Project.Structure.Labels;
+         //RobotLabel labelRSA;
+         RobotNamesArray namArrRSA = labelsRSA.GetAvailableNames(IRobotLabelType.I_LT_MATERIAL);
+         string[] tmp = new string[namArrRSA.Count];
+         for (int i = 0; i < namArrRSA.Count; i++)
+         {
+            tmp[i] = namArrRSA.Get(i + 1);
+         }
+         MaterialsRSA = tmp;
+      }
+      public void getMaterialNameListRSA()
+      {
+         RobotApplication appRSA = new RobotApplication();
+         RobotLabelServer labelsRSA = appRSA.Project.Structure.Labels;
+         //RobotLabel labelRSA;
+         RobotNamesArray namArrRSA = labelsRSA.GetAvailableNames(IRobotLabelType.I_LT_MATERIAL);
+         string[] tmp = new string[namArrRSA.Count];
+         for (int i = 0; i < namArrRSA.Count; i++)
+         {
+            tmp[i] = namArrRSA.Get(i + 1);
+         }
+         MaterialsRSA = tmp;
+      }
+
+      public void test()
+      {
+         RobotApplication appRSA = new RobotApplication();
+         getMaterialNameListRSA(appRSA);
+         getBarSectionNamesRSA(appRSA);
+         RobotStructure structureRSA = appRSA.Project.Structure;
+         RobotBarServer barsRSA = structureRSA.Bars;
+         RobotNodeServer nodesRSA = structureRSA.Nodes;
+         RobotObjObjectServer objRSA = structureRSA.Objects;
+
+         RobotLabelServer labelsRSA = appRSA.Project.Structure.Labels;
+
+         //Определение выборок выделенных в модели объектов
+         RobotSelection barSel = structureRSA.Selections.Get(IRobotObjectType.I_OT_BAR);
+         RobotSelection plateSel = structureRSA.Selections.Get(IRobotObjectType.I_OT_PANEL);
+         RobotSelection holesSel = structureRSA.Selections.Get(IRobotObjectType.I_OT_GEOMETRY);
+
+         RobotBarCollection barsMany = (RobotBarCollection)barsRSA.GetMany(barSel);
+         RobotObjObjectCollection platesMany = (RobotObjObjectCollection)objRSA.GetMany(plateSel);
+         IRobotBar barRSA = barsMany.Get(3);
+         IRobotObjObject plate = platesMany.Get(1);
+
+         IRobotLabel labelPlate = plate.GetLabel(IRobotLabelType.I_LT_PANEL_THICKNESS);
+         RobotGeoPoint3DCollection defPoints= (RobotGeoPoint3DCollection)plate.Main.DefPoints;
+         RobotGeoPoint3D pt1 = defPoints.Get(1);
+         RobotGeoPoint3D pt2 = defPoints.Get(2);
+         RobotGeoPoint3D pt3 = defPoints.Get(defPoints.Count);
+         //IRobotGeoContour geoContour=(IRobotGeoContour)plate.Main.Geometry;
+         //IRobotGeoObject geoObj=plate.Main.Geometry;
+
+         IRobotLabel labelBar = barRSA.GetLabel(IRobotLabelType.I_LT_BAR_SECTION);
+         RobotBarSectionData data = labelBar.Data;
+         RobotBarSectionConcreteData cData = data.Concrete;
+         double b = cData.GetValue(IRobotBarSectionConcreteDataValue.I_BSCDV_COL_B);
+         double h = cData.GetValue(IRobotBarSectionConcreteDataValue.I_BSCDV_COL_H);
+         double l1 = cData.GetValue(IRobotBarSectionConcreteDataValue.I_BSCDV_COL_L1);
+         double l2 = cData.GetValue(IRobotBarSectionConcreteDataValue.I_BSCDV_COL_L2);
+         double h1 = cData.GetValue(IRobotBarSectionConcreteDataValue.I_BSCDV_COL_H1);
+         double h2 = cData.GetValue(IRobotBarSectionConcreteDataValue.I_BSCDV_COL_H2);
+         double de = cData.GetValue(IRobotBarSectionConcreteDataValue.I_BSCDV_COL_DE);
+         double alfa = barRSA.Gamma;
+      }
+   }
+}
---
+++ ObjCreator.cs
@@ -0,0 +1,53 @@
+﻿using RobotOM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spider_Free
+{
+   class ObjCreator
+   {
+
+      public static int AddNode(RobotStructure structureRSA, double x, double y, double z)
+      {
+         RobotNodeServer nodesRSA = structureRSA.Nodes;
+         int freeNum = nodesRSA.FreeNumber;
+         nodesRSA.Create(freeNum, x, y, z);
+         return freeNum;
+      }
+
+      public static void AddBar(RobotStructure structureRSA, RobotNode node1, RobotNode node2, IRobotLabel labelSect)
+      {
+         int num1 = node1.Number;
+         int num2 = node2.Number;
+         RobotBarServer barsRSA = structureRSA.Bars;
+         int freeNum = barsRSA.FreeNumber;
+         barsRSA.Create(freeNum, num1, num2);
+         RobotSelection selection = structureRSA.Selections.Create(IRobotObjectType.I_OT_BAR);
+         selection.FromText(freeNum.ToString());
+         barsRSA.SetLabel(selection, IRobotLabelType.I_LT_BAR_SECTION, labelSect.Name);
+      }
+      
+      public static int AddBar(RobotStructure structureRSA, RobotNode node1, RobotNode node2)
+      {
+         int num1 = node1.Number;
+         int num2 = node2.Number;
+         RobotBarServer barsRSA = structureRSA.Bars;
+         int freeNum = barsRSA.FreeNumber;
+         barsRSA.Create(freeNum, num1, num2);
+         return freeNum;
+      }
+
+      public static void AddHole()
+      {
+
+      }
+
+      public static void AddRigidLink()
+      {
+
+      }
+   }
+}
